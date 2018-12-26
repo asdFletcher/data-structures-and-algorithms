4a98a5631024c8cf92c7a12e00050380d80030f0
Fletcher LaRue <laruefletcher@gmail.com>
2018-12-25 23:26:38 -0800

    readme

--- code-challenges/linked_list/linked-list.js
+++ code-challenges/linked_list/linked-list.js
@@ -1,71 +1,71 @@
 'use strict';
 
 class Node {
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class LinkedList {
 
-    constructor(){
-        this.head = null;
-    }
+  constructor(){
+    this.head = null;
+  }
 
-    add(value){
-        let node = new Node(value);
-        // if first node, make it the head
-        if (this.head === null){
-            this.head = node;
-            return;
-        }
-        // if it is not empty
-        // go to the end and create a new node
-        // go until current.next === null;
+  add(value){
+    let node = new Node(value);
+    // if first node, make it the head
+    if (this.head === null){
+      this.head = node;
+      return;
+    }
+    // if it is not empty
+    // go to the end and create a new node
+    // go until current.next === null;
+  
+    let current = this.head;
+    while(current.next !== null){
+      current = current.next;
+    }
+    current.next = node;
+  }
+
+  insert(value){
+    let oldHead = this.head;
+    let newHead = new Node(value);
+    this.head = newHead;
+    newHead.next = oldHead;
+  }
+
+  includes(value){
       
-        let current = this.head;
-        while(current.next !== null){
-            current = current.next;
-        }
-        current.next = node;
+    let included = false;
+    if(this.head === null){
+      return included;
+    }
+    let current = this.head;
+    if (current.value === value) {return true; }   
+    while(current.next !== null){
+      current = current.next;
+      if (current.value === value) {return true; }   
+    }
+    return false;
+  }
+
+  print(){
+    if (this.head === null){
+      console.log('undefined');
+      return null;
     }
 
-    insert(value){
-        let oldHead = this.head;
-        let newHead = new Node(value);
-        this.head = newHead;
-        newHead.next = oldHead;
+    let current = this.head;
+    console.log(current.value);
+    while(current.next !== null){
+      current = current.next;
+      console.log(current.value);
     }
-
-    includes(value){
-        
-        let included = false;
-        if(this.head === null){
-            return included;
-        }
-        let current = this.head;
-        if (current.value === value) {return true; }   
-        while(current.next !== null){
-            current = current.next;
-            if (current.value === value) {return true; }   
-        }
-        return false;
-    }
-
-    print(){
-        if (this.head === null){
-            console.log('undefined');
-            return null;
-        }
-
-        let current = this.head;
-        console.log(current.value);
-        while(current.next !== null){
-            current = current.next;
-            console.log(current.value);
-        }
-    }
+  }
  
 }
 
