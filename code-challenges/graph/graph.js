19ea21e78cc6ac14c67bc2a287fae682b10f67d2
Fletcher LaRue <laruefletcher@gmail.com>
2019-02-09 19:12:30 -0800

    adds graph implementation

---
+++ code-challenges/graph/graph.js
@@ -0,0 +1,51 @@
+'use strict';
+
+class Node{
+  constructor(value){
+    this.value = value;
+  }
+}
+
+class Graph {
+  constructor(){
+    this.nodeCount = 0;
+    this.nodeList = new Map();
+  }
+
+  addNode(value) {
+    let node = new Node(value);
+    this.nodeList.set(node, new Map());
+    this.nodeCount++;
+    return node;
+  }
+
+  addEdge(startNode, endNode, weight = 0) {
+    let startNodeMap = this.nodeList.get(startNode);
+    let endNodeMap = this.nodeList.get(endNode);
+    if(!startNodeMap || !endNodeMap){
+      throw new Error(`__ERROR__ invalid nodes`);
+    }
+    startNodeMap.set(endNode, weight);
+  }
+
+  getNodes(){
+    return this.nodeList;
+  }
+
+  getNeighbors(node){
+    let neighborMap = this.nodeList.get(node);
+    if (!neighborMap){
+      throw new Error(`__ERROR__ invalid nodes`);
+    }
+    return neighborMap;
+  }
+
+  size(){
+    return this.nodeCount;
+  }
+}
+
+module.exports = {
+  Graph,
+  Node,
+};
