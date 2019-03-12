8e8109f34e20cd115c3fd15766464ec1d68afbc9
Fletcher LaRue <laruefletcher@gmail.com>
2019-03-12 15:30:20 -0700

    moves datastructure sto their own folder

--- code-challenges/arrays/arrayReverse/array-reverse.js
+++ code-challenges/arrays/arrayReverse/array-reverse.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const test1 = [1,2,3];
 const test2 = [];
@@ -6,20 +6,20 @@ const test3 = 'string';
 
 const reverseArray = (arr) => {
 
-   if(Array.isArray(arr) === false){
-       return 'not an array'
-   }
-   if(arr.length === 0){
-       return arr
-   }
-   let reverse = [];
-   let counter = 0;
-   for(let i = arr.length-1; i>=0; i--){
-       reverse[counter] = arr[i];
-       counter ++;
-   }
-   return reverse;
-}
+  if(Array.isArray(arr) === false){
+    return 'not an array'
+  }
+  if(arr.length === 0){
+    return arr;
+  }
+  let reverse = [];
+  let counter = 0;
+  for(let i = arr.length-1; i>=0; i--){
+    reverse[counter] = arr[i];
+    counter ++;
+  }
+  return reverse;
+};
 
 console.log('expect [3,2,1]: ' + reverseArray(test1));
 console.log('expect []: ' + reverseArray(test2));
