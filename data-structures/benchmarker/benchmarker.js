e55238a6c3423117d75216876482d0a3dd1b3d16
Fletcher LaRue <laruefletcher@gmail.com>
2019-03-18 14:42:46 -0700

    wtf

--- data-structures/benchmarker/benchmarker.js
+++ data-structures/benchmarker/benchmarker.js
@@ -3,33 +3,28 @@
 const AVLTreeNode = require('../avl-tree/avl-tree-node.js');
 const AVLTree = require('../avl-tree/avl-tree.js');
 
+function doStuff() {
+  let count = 0;
+  const times = {};
+  const data = [];
 
-let count = 0;
-const times = {};
-const data = [];
+  async function test(){
+    let time = new Date().getTime();
+    times[time] = count;
+    count++;
+    for(let i = 0; i < 100; i++){
 
-
-const myTimer = setInterval( ()=> {
-  let time = new Date().getTime();
-  times[time] = count;
-  count++;
-  if (count > 1000){
-    clearInterval(myTimer);
-    
-
-    for( let key in times){
-      data.push({x: times[key], y: key});
     }
-    // console.log(times);
-    // console.log(data);
+      for( let key in times){
 
-    console.log(data[data.length-1].y - data[0].y);
   }
-}, 0);
 
+  await test();
+  data.push({x: times[key], y: key});
+  // console.log(times);
+  // console.log(data);
+  console.log(data[data.length-1].y - data[0].y);
 
+}
 
-
-
-
-
+doStuff();
