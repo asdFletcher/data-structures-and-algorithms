39708f9d4fca022da00c59bb75c2e3917fb7d6fd
Fletcher LaRue <laruefletcher@gmail.com>
2019-03-19 03:41:25 -0700

    basic plotting of AVL Tree insert method, scatter plot

--- data-structures/benchmarker/benchmarker.js
+++ data-structures/benchmarker/benchmarker.js
@@ -3,28 +3,70 @@
 const AVLTreeNode = require('../avl-tree/avl-tree-node.js');
 const AVLTree = require('../avl-tree/avl-tree.js');
 
-function doStuff() {
-  let count = 0;
-  const times = {};
-  const data = [];
 
-  async function test(){
-    let time = new Date().getTime();
-    times[time] = count;
-    count++;
-    for(let i = 0; i < 100; i++){
+let plotPoints = [];
+const n = 60000;
+// const plotPointRate = 1000;
+const sampleRate = 1000;
+const manyRandomNumbers = new Set();
 
-    }
-      for( let key in times){
-
+function generateRandomNumbers(){
+  while (manyRandomNumbers.size < n){
+    let num = Math.floor(Math.random() * 1024 * 1024 * 128);
+    manyRandomNumbers.add(num);
   }
+}
 
-  await test();
-  data.push({x: times[key], y: key});
-  // console.log(times);
-  // console.log(data);
-  console.log(data[data.length-1].y - data[0].y);
+let times = new Array(n);
+
+function runTheTest(){
+  const myTree = new AVLTree();
+  
+  let i = 0;
+  let ti = new Date().getTime();
+  manyRandomNumbers.forEach( val => {
+    
+    // do the insertion
+    myTree.insert(val);
+
+    if (i % sampleRate === 0) {
+      let tf = new Date().getTime();
+      let elapsed = (tf-ti);
+      // console.log(`added ${sampleRate} elements in: ${elapsed} ms`);
+
+      times[i] = elapsed;
+      
+      ti = new Date().getTime();
+    }
+    i++;
+  });
+}
+
+function postProcess(){
+  for(let i = 0 ; i < times.length; i++){
+    if (times[i] !== undefined) {
+      plotPoints.push({x: i, y: times[i]});
+    }
+  }
 
 }
 
+
+function doStuff(){
+  console.log(`generating numbers 🥩`);
+  generateRandomNumbers();
+  console.log(`running the test 🥕`);
+  let totalStart = new Date().getTime();
+  runTheTest();
+  let totalEnd = new Date().getTime();
+  console.log(`Test complete, 🍌`);
+  postProcess();
+  console.log(`Post processing complete 🥦!`);
+  // console.log(JSON.stringify(plotPoints))
+  console.log(plotPoints);
+  console.log(`total Time: `, totalEnd-totalStart);
+  // console.log(`${data[data.length-1].y - data[0].y} ms`);
+}
+
 doStuff();
+
