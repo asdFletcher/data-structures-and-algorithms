7ef77adea3babb98053d8548910322c510f18d66
Fletcher LaRue <laruefletcher@gmail.com>
2019-04-22 03:55:01 -0700

    basic splay tree insert method work

--- src/data-structures/splay-tree/__tests__/splay-tree.test.js
+++ src/data-structures/splay-tree/__tests__/splay-tree.test.js
@@ -2,5 +2,106 @@
 
 const SplayTree = require('../splay-tree.js');
 
+describe('splay tree', () => {
+  describe('constructor', () => {
+    it('creates empty tree without error', () => {
+      expect( () => {
+        const myTree = new SplayTree();
+      }).not.toThrow();
+    });
+    it('new tree is of correct instance type', () => {
+        const myTree = new SplayTree();
+        expect(myTree).toBeInstanceOf(SplayTree);
+    });
+    it('root is null', () => {
+        const myTree = new SplayTree();
+        expect(myTree.root).toBeNull();
+    });
+    it('arguments are ignored', () => {
+        const myTree = new SplayTree(2);
+        expect(myTree).toBeInstanceOf(SplayTree);
+        expect(myTree.root).toBeNull();
+    });
+  });
 
+  describe('insert', () => {
+    it('fails on no arguments', () => {
+      const myTree = new SplayTree();
+      let result = myTree.insert();
+
+      expect(myTree.root).toBeNull();
+      expect(result).toBeUndefined();
+    });
+    it('fails on boolean input', () => {
+      const myTree = new SplayTree();
+      myTree.insert(true);
+      let result = myTree.insert(false);
+
+      expect(myTree.root).toBeNull();
+      expect(result).toBeUndefined();
+    });
+    it('fails on string input', () => {
+      const myTree = new SplayTree();
+      myTree.insert('string');
+      let result = myTree.insert('true');
+
+      expect(myTree.root).toBeNull();
+      expect(result).toBeUndefined();
+    });
+    it('succeeds on integer input', () => {
+      const myTree = new SplayTree();
+      let result = myTree.insert(1);
+
+      expect(myTree.root).toBeInstanceOf(Node);
+      expect(myTree.root.value).toEqual(1);
+      expect(result).toBeInstanceOf(Node);
+      expect(result.value).toBe(1);
+    });
+    it('succeeds on float input', () => {
+      const myTree = new SplayTree();
+      let result = myTree.insert(1.1);
+
+      expect(myTree.root).toBeInstanceOf(Node);
+      expect(myTree.root.value).toEqual(1.1);
+      expect(result).toBeInstanceOf(Node);
+      expect(result.value).toBe(1.1);
+    });
+    it('succeeds on negative number input', () => {
+      const myTree = new SplayTree();
+      let result = myTree.insert(-5);
+
+      expect(myTree.root).toBeInstanceOf(Node);
+      expect(myTree.root.value).toEqual(-5);
+      expect(result).toBeInstanceOf(Node);
+      expect(result.value).toBe(-5);
+    });
+    it('succeeds on integer input as string', () => {
+      const myTree = new SplayTree();
+      let result = myTree.insert('2');
+
+      expect(myTree.root).toBeInstanceOf(Node);
+      expect(myTree.root.value).toEqual(2);
+      expect(result).toBeInstanceOf(Node);
+      expect(result.value).toBe(2);
+    });
+    it('succeeds on float input as string', () => {
+      const myTree = new SplayTree();
+      let result = myTree.insert('2.2');
+
+      expect(myTree.root).toBeInstanceOf(Node);
+      expect(myTree.root.value).toEqual(2.2);
+      expect(result).toBeInstanceOf(Node);
+      expect(result.value).toBe(2.2);
+    });
+    it('succeeds on negative input as string', () => {
+      const myTree = new SplayTree();
+      let result = myTree.insert('-2');
+
+      expect(myTree.root).toBeInstanceOf(Node);
+      expect(myTree.root.value).toEqual(-2);
+      expect(result.value).toBe(-2);
+    });
+
+  });
+});
 
--- src/data-structures/splay-tree/splay-tree.js
+++ src/data-structures/splay-tree/splay-tree.js
@@ -5,14 +5,13 @@ const Node = require('./splay-tree-node.js');
 
 class SplayTree {
   constructor() {
-    this.value = 1;
+    this.root = null;
   }
 
-  insert(value) {
-    if (!this.isNumericInput(value)) {
-      return undefined;
-    }
-    const cleanValue = Number(value);
+  insert(rawValue) {
+    if (!this.isNumericInput(rawValue)) { return undefined; }
+    
+    const value = Number(rawValue);
 
     const newNode = new Node(value);
 
@@ -21,8 +20,131 @@ class SplayTree {
       return newNode;
     }
 
-        
+    const path = [];
+    let result = undefined;
+
+    const go = (node) => {
+      path.push(node);
+      if (value < node.value) {
+        if (!node.left) { // base case
+          node.left = newNode;
+          result = newNode;
+          path.push(newNode);
+          return;
+        }
+        go(node.left);
+      } else if (value > node.value) {
+        if (!node.right) { // base case
+          node.right = newNode;
+          result = newNode;
+          path.push(newNode);
+          return;
+        }
+        go(node.right);
+      } else {
+        return; // duplicate value
+      }
+    }
+
+    go(this.root);
+
+    if (result === undefined) { return result; }
+    
+    // perform splay
+    this.splay(path);
+
+    return result;
   }
+
+  rightSingle(target, parent) {
+    parent.left = target.right;
+    target.right = parent;
+    return target;
+  }
+
+  // case 
+  //      R
+  //        T
+  leftSingle(target, parent) {
+    parent.right = target.left;
+    target.left = parent;
+    return target;
+  }
+
+  splay(path) {
+    let targetIndex = path.length - 1;
+    let target = path[targetIndex];
+    
+    let parentIndex = path.length - 2;
+    let parent = path[parentIndex];
+
+    let grandParentIndex = path.length - 3;
+    let grandParent = path[grandParentIndex];
+    
+    while (this.root !== target) {
+      // case 
+      //      R
+      //    T
+      if (parent === this.root && parent.left === target) {
+        this.root.left = this.rightSingle(target, parent);
+      }
+      // case 
+      //      R
+      //        T
+      if (parent === this.root && parent.right === target) {
+        this.root.right = this.leftSingle(target, parent);
+      }
+
+      // case 
+      //      R
+      //    P
+      //  T
+      if (grandParent.left === parent && parent.left === target) {
+        grandParent.left = this.rightSingle(target, parent);
+        parentIndex -= 1;
+        parent = path[parentIndex];
+        grandParentIndex -= 1;
+        grandParent = path[grandParentIndex];
+      }
+      // case 
+      //      R
+      //        P
+      //          T
+      if (grandParent.right === parent && parent.right === target) {
+        grandParent.right = this.leftSingle(target, parent);
+        parentIndex -= 1;
+        parent = path[parentIndex];
+        grandParentIndex -= 1;
+        grandParent = path[grandParentIndex];
+      }
+      // case 
+      //      G
+      //    P
+      //     T
+      if (grandParent.left === parent && parent.right === target) {
+        grandParent.left = this.leftSingle(target, parent);
+        parentIndex -= 1;
+        parent = path[parentIndex];
+        grandParentIndex -= 1;
+        grandParent = path[grandParentIndex];
+      }
+      // case 
+      //      G
+      //        P
+      //       T
+      if (grandParent.right === parent && parent.left === target) {
+        grandParent.right = this.rightSingle(target, parent, grandParent);
+        parentIndex -= 1;
+        parent = path[parentIndex];
+        grandParentIndex -= 1;
+        grandParent = path[grandParentIndex];
+      }
+    }
+
+
+  }
+
+
 
   treeIsEmpty() {
     if (!this.root) { return true; }
